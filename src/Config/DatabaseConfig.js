038a975003351e3e2f9520d05a568e771a0cf6f5
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-30 19:43:33 +0000

    first commit

---
+++ src/Config/DatabaseConfig.js
@@ -0,0 +1,12 @@
+const { Sequelize } = require('sequelize');
+
+const database = new Sequelize('TrainingGuru', 'root', 'root', {
+    dialect: 'mysql',
+    host: 'localhost',
+    port: '8889'
+
+});
+
+
+module.exports = database;
+
