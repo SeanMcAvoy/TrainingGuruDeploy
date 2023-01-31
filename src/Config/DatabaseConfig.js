49acc249d18ded5e3ae00b6cf436bde4f6eac1e4
seanmcavoy <smcavoy.mcavoy@gmail.com>
2023-01-31 22:36:06 +0000

    changes for deployment

--- src/Config/DatabaseConfig.js
+++ src/Config/DatabaseConfig.js
@@ -1,12 +1,17 @@
 const { Sequelize } = require('sequelize');
+require('dotenv').config();
 
-const database = new Sequelize('TrainingGuru', 'root', 'root', {
+const database = new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER_NAME, process.env.DATABASE_PW, {
     dialect: 'mysql',
-    host: 'localhost',
-    port: '8889'
+    host: process.env.DATABASE_URL,
+    port: '3306'
 
 });
 
 
 module.exports = database;
+
+
+
+
 
